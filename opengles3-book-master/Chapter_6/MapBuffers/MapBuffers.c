c554208041468d277fdb6d8c40039264b743c4c1
zhangyiming <2283942163@qq.com>
2016-05-06 16:10:02 +0800

    map buffer

--- opengles3-book-master/Chapter_6/MapBuffers/MapBuffers.c
+++ opengles3-book-master/Chapter_6/MapBuffers/MapBuffers.c
@@ -80,7 +80,9 @@ char * ReadFile (char * filename) {
         fseek(handler, 0, SEEK_END);
         string_size = ftell(handler);
         rewind(handler);
+        buffer = (char *)malloc(sizeof(char) * (string_size + 1));
         read_size = fread(buffer, sizeof(char), string_size, handler);
+        buffer[string_size + 1] = '\0';
         if (read_size != string_size) {
             free(buffer);
             buffer = NULL;
@@ -92,10 +94,12 @@ char * ReadFile (char * filename) {
 int Init (ESContext * esContext)
 {
     UserData * userData = esContext->userData;
-    const char * vShaderStr = ReadFile("/Users/jiangchenrui/Desktop/opengles3-book-master/Chapter_6/MapBuffers/iOS/MapBuffers/MapBuffers/Shader.vsh");
-    const char * fShaderStr = ReadFile("/Users/jiangchenrui/Desktop/opengles3-book-master/Chapter_6/MapBuffers/iOS/MapBuffers/MapBuffers/Shader.fsh");
+    const char * vShaderStr = ReadFile("/Users/jiangchenrui/opengl-es-practise/opengles3-book-master/Chapter_6/MapBuffers/iOS/MapBuffers/MapBuffers/Shader.vsh");
+    const char * fShaderStr = ReadFile("/Users/jiangchenrui/opengl-es-practise/opengles3-book-master/Chapter_6/MapBuffers/iOS/MapBuffers/MapBuffers/Shader.fsh");
     
-    GLuint programObject = esLoadProgram(vShaderStr, fShaderStr);
+    
+    GLuint programObject ;
+    programObject = esLoadProgram(vShaderStr, fShaderStr);
     if (programObject == 0) {
         return GL_FALSE;
     }
@@ -103,29 +107,106 @@ int Init (ESContext * esContext)
     userData->programObject = programObject;
     userData->vboIds[0] = 0;
     userData->vboIds[1] = 0;
+    
     glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    
     return GL_TRUE;
+    
 }
 
-
-void DrawPrimitiveWithVBOsMapBuffers(ESContext *esContext,
-                                     GLint numVertices,
-                                     GLfloat * vtxBuf,
-                                     GLint vtxStride,
-                                     GLint numIndices,
-                                     GLushort * indices) {
-    UserData * userData = esContext->userData;
+/**
+ *  使用map
+ *
+ *  @param esContext   escontext
+ *  @param numVertices 3
+ *  @param vtxBuf      vertices 位置和颜色的集合
+ *  @param vtxStride   sizeof ( GLfloat ) * ( VERTEX_POS_SIZE + VERTEX_COLOR_SIZE )
+ *  @param numIndices  3
+ *  @param indices     ｛0，1，2｝
+ */
+void DrawPrimitiveWithVBOsMapBuffers (ESContext * esContext,
+                                      GLuint numVertices,
+                                      GLfloat * vtxBuf,
+                                      GLfloat vtxStride,
+                                      GLint numIndices,
+                                      GLushort * indices){
+    UserData * userdata = esContext->userData;
     GLuint offset = 0;
-    if (userData->vboIds[0] == 0 && userData->vboIds[1] == 0) {
+    if (userdata->vboIds[0] == 0 && userdata->vboIds[1] == 0) {
         GLfloat * vtxMappedBuf;
         GLushort * idxMappedBuf;
+        glGenBuffers(2, userdata->vboIds);
+        glBindBuffer(GL_ARRAY_BUFFER, userdata->vboIds[0]);
+        glBufferData(GL_ARRAY_BUFFER, vtxStride * numIndices, NULL, GL_STATIC_DRAW);
         
-        glGenBuffers(<#GLsizei n#>, <#GLuint *buffers#>)
+        vtxMappedBuf = (GLfloat *)glMapBufferRange(GL_ARRAY_BUFFER, 0, vtxStride * numVertices, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+        if (vtxMappedBuf == NULL) {
+            esLogMessage("error mapping vertex buffer object");
+            return;
+        }
         
+        memcpy(vtxMappedBuf, vtxBuf, vtxStride * numVertices);
+        
+        if (glUnmapBuffer(GL_ARRAY_BUFFER) == GL_FALSE) {
+            esLogMessage("error unmapping array buffer object");
+            return;
+        }
+        
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, userdata->vboIds[1]);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLushort) * numIndices, NULL, GL_STATIC_DRAW);
+        idxMappedBuf = (GLushort *) glMapBufferRange(GL_ELEMENT_ARRAY_BUFFER, 0, sizeof(GLushort) * numIndices, GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT);
+        if (idxMappedBuf == NULL) {
+            esLogMessage("error mapping element array buffer object.");
+            return;
+        }
+        
+        memcpy(idxMappedBuf, indices, sizeof(GLushort) * numIndices);
+        
+        if(glUnmapBuffer(GL_ELEMENT_ARRAY_BUFFER ) == GL_FALSE) {
+            esLogMessage("error mapping element array buffer object.");
+        }
     }
+    
+    
 }
 
+void Draw (ESContext * esContext) {
+    UserData * userData = esContext->userData;
+    GLfloat vertices[3 * ( VERTEX_POS_SIZE + VERTEX_COLOR_SIZE )] =
+    {
+        0.0f,  0.5f, 0.0f,        // v0
+        1.0f,  0.0f, 0.0f, 1.0f,  // c0
+        -0.5f, -0.5f, 0.0f,        // v1
+        0.0f,  1.0f, 0.0f, 1.0f,  // c1
+        0.5f, -0.5f, 0.0f,        // v2
+        0.0f,  0.0f, 1.0f, 1.0f,  // c2
+    };
+    
+    GLushort indice[3] = {0,1,2};
+    glViewport(0, 0, esContext->width, esContext->height);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(userData->programObject);
+    DrawPrimitiveWithVBOsMapBuffers(esContext, 3,vertices
+                                    , sizeof(GLfloat) * (VERTEX_POS_SIZE + VERTEX_COLOR_SIZE), 3, indice);
+    
+}
 
+void ShutDown (ESContext * esContext) {
+    UserData * userdata = esContext->userData;
+    glDeleteProgram(userdata->programObject);
+    glDeleteBuffers(2, userdata->vboIds);
+}
+
+int esMain (ESContext * esContext) {
+    esContext->userData = malloc(sizeof(UserData));
+    esCreateWindow(esContext, "vertexBufferObjects", 320, 240, ES_WINDOW_RGB);
+    if (!Init(esContext)) {
+        return GL_FALSE;
+    }
+    esRegisterShutdownFunc(esContext, ShutDown);
+    esRegisterDrawFunc(esContext, Draw);
+    return GL_TRUE;
+}
 
 
 
